4c6233369ae09a2a733ceb1ee11d65b6fc6015e8
Muhammad Rheza Alfin <aRhez0903@gmail.com>
2024-11-05 21:13:38 +0700

    add api routes

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function (Request $request) {
+    return response()->json([
+        'status' => 'success',
+    ]);
+});
